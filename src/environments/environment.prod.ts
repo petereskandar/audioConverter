0a48c6aceaca43086d42be7cc423fc329c193bd5
Peter <eskandar.peter@accenture.com>
2021-01-20 03:16:13 +0100

    add s3 lambda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  REGION: 'eu-west-3',
-  IDENTITY_POOL_ID: "eu-west-3:8b3c9480-e3fb-4d5c-90a7-055f8d73f9a5",
-  ROLEARN: 'arn:aws:iam::250950161175:role/Cognito_demoIdPoolUnauth_Role',
-  BUCKET: 'aws-devops-peter-eskandar',
+  REGION: '',
+  IDENTITY_POOL_ID: "",
+  ROLEARN: '',
+  BUCKET: '',
   production: true
 };
