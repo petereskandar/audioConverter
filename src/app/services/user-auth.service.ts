36de4a5f468668005456a41136937b95706b7d46
Peter <eskandar.peter@accenture.com>
2021-01-27 22:10:58 +0100

    update html

--- src/app/services/user-auth.service.ts
+++ src/app/services/user-auth.service.ts
@@ -1,7 +1,7 @@
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { CognitoUserInterface } from '@aws-amplify/ui-components';
-import {Auth} from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 import * as AWS from 'aws-sdk';
 
 @Injectable({
@@ -13,20 +13,31 @@ export class UserAuthService {
   constructor() { }
 
   async getUserAuthorized() {
-    // cognito identity pool --> unauthorized access
-    AWS.config.region = environment.REGION;
-    AWS.config.credentials = await new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: environment.IDENTITY_POOL_ID,
-      RoleArn: environment.ROLEARN
-    });
-
     return new Promise((resolve, reject) => resolve(true));
   }
 
   // save logged user data
-  setLoggedUser(user: CognitoUserInterface | undefined) {
-    this.user = user;
+  setLoggedUser(cognitoUser: CognitoUserInterface | undefined) {
+    this.user = cognitoUser;
+    this.setAWSCredentials(this.user);
     localStorage.setItem('user', JSON.stringify(this.user));
+  }
+
+  // set AWS Security & Access tokens
+  setAWSCredentials(cognitoUser: CognitoUserInterface | undefined) {
+    AWS.config.region = environment.REGION;
+    if(cognitoUser != null) {
+      cognitoUser.getSession((err, result) => {
+        if(result) {
+          AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'eu-west-3:fd5f34c8-610d-4dee-89fb-f864398cd55e',
+            Logins: {
+              'cognito-idp.eu-west-3.amazonaws.com/eu-west-3_pNetY4Lfm' : result.getIdToken().getJwtToken()
+            }
+          });
+        }
+      });
+    }
   }
 
   get loggedUser() {
